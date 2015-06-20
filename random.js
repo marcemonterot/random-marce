2f13e4f74b62e04b11b405dd6b7df82e0fcbcfd4
marcemonterot <marce@marce-portatil.marce.com>
2015-06-20 16:18:06 +0200

    ramas 2
    nueva version para resolver conflictos

--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 var numero=Math.random();
 
-if (numero<0.5){
-	console.log('numero '+numero+' menor que 0,5');
+if (numero<0.75){
+	console.log('numero '+numero+' menor que 0,75');
 }
 else
 {
-	console.log('numero '+numero+' mayor que 0,5');
+	console.log('numero '+numero+' mayor que 0,75');
 }
